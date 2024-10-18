8deeb93c7b45dadf80f18925182f6d1ce9d29377
Anicai Ovidiu <ovimihai@gmail.com>
2024-10-18 23:08:36 +0300

    pong, paddles, ball, interaction

---
+++ images.g.ts
@@ -0,0 +1,77 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "right paddle":return img`
+1 1 1 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 8 8 1 
+1 1 1 1 
+`;
+            case "image1":
+            case "left paddle":return img`
+1 1 1 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 2 2 1 
+1 1 1 1 
+`;
+            case "image3":
+            case "myImage":return img`
+. . 1 1 1 1 1 1 1 . . 
+. 1 f f f f f f f 1 . 
+1 f . 5 5 5 5 5 . f 1 
+1 f 5 1 1 1 1 1 5 f 1 
+1 f 5 1 1 1 1 1 5 f 1 
+1 f 5 1 1 1 1 1 5 f 1 
+1 f 5 1 1 1 1 1 5 f 1 
+1 f 5 1 1 1 1 1 5 f 1 
+1 f . 5 5 5 5 5 . f 1 
+. 1 f f f f f f f 1 . 
+. . 1 1 1 1 1 1 1 . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,37 @@
-
+namespace SpriteKind {
+    export const Ball = SpriteKind.create()
+}
+// reverse horizontal speed
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Ball, function (sprite, otherSprite) {
+    if (otherSprite.x < 100) {
+        otherSprite.left = sprite.right
+    } else {
+        otherSprite.right = sprite.left
+    }
+    otherSprite.vx = -1 * otherSprite.vx
+})
+function create_right_paddle () {
+    right_paddle = sprites.create(assets.image`right paddle`, SpriteKind.Player)
+    right_paddle.x = 150
+    right_paddle.setStayInScreen(true)
+    controller.player2.moveSprite(right_paddle, 0, 50)
+}
+function create_left_paddle () {
+    left_paddle = sprites.create(assets.image`left paddle`, SpriteKind.Player)
+    left_paddle.x = 10
+    left_paddle.setStayInScreen(true)
+    controller.moveSprite(left_paddle, 0, 50)
+}
+function create_ball () {
+    ball = sprites.create(assets.image`myImage`, SpriteKind.Ball)
+    ball.setPosition(scene.screenWidth() / 2, scene.screenHeight() / 2)
+    ball.setStayInScreen(true)
+    ball.setBounceOnWall(true)
+    ball.setVelocity(50, 50)
+}
+let ball: Sprite = null
+let left_paddle: Sprite = null
+let right_paddle: Sprite = null
+create_left_paddle()
+create_right_paddle()
+create_ball()
